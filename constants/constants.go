603226b7ca040344fb1dabac15cc3b5ae1b05cde
stnokott <nftrash@web.de>
2022-11-30 13:19:48 +0100

    feat: add version to user agent

---
+++ constants/constants.go
@@ -0,0 +1,5 @@
+package constants
+
+const VERSION string = "v0.1.0"
+
+const USER_AGENT string = "inofficial private non-commercial stats API " + VERSION
--- request/request.go
+++ request/request.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/pkg/errors"
+	"github.com/stnokott/r6api/constants"
 )
 
 type ubiErrResp struct {
@@ -17,7 +18,7 @@ type ubiErrResp struct {
 // Plain executes r with the default HTTP client and returns the plain body.
 // Remember to close it after reading.
 func Plain(r *http.Request) (io.ReadCloser, error) {
-	r.Header.Add("User-Agent", "inofficial private non-commercial stats API (nfkottenhahn@web.de)")
+	r.Header.Add("User-Agent", constants.USER_AGENT)
 	resp, err := http.DefaultClient.Do(r)
 	if err != nil {
 		return nil, err
@@ -28,7 +29,7 @@ func Plain(r *http.Request) (io.ReadCloser, error) {
 // JSON executes r with the default HTTP client and performs API-related processing such as deserialization and error-checking.
 // If no errors occur, it attempts to unmarshal the response body into dst.
 func JSON(r *http.Request, dst any) (err error) {
-	r.Header.Add("User-Agent", "inofficial private non-commercial stats API (nfkottenhahn@web.de)")
+	r.Header.Add("User-Agent", constants.USER_AGENT)
 	r.Header.Add("Accept", "application/json")
 	var resp *http.Response
 	resp, err = http.DefaultClient.Do(r)
